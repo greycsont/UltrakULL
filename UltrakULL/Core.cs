817fcc834812c48cafab653f56dcf66d3d9f6f22
greycsont <RuiD2019@outlook.com>
2026-07-14 01:24:48 +0100

    Decoupled Core.HandleSceneSwitch to make it readable

--- UltrakULL/Core.cs
+++ UltrakULL/Core.cs
@@ -17,21 +17,13 @@ public static class Core
 {
 	public static bool wasLanguageReset = false;
 
-    //Encapsulation function to patch all of the front end.
-    public static void PatchFrontEnd(GameObject frontEnd)
-    {
-        MainMenu.Patch(frontEnd);
-        Options.Patch(ref frontEnd);
-    }
-    
     public static void HandleSceneSwitch(Scene scene,ref GameObject canvas)
     {
-
-        //Logging.Message("Switching scenes...");
         string levelName = GetCurrentSceneName();
+
+        // Don't do anything if we're still booting up the game.
         if(levelName == "Intro" || levelName == "Bootstrap")
         { 
-            //Don't do anything if we're still booting up the game.
             //Logging.Warn("In intro, not hooking yet");
             return;
         }
@@ -43,109 +35,117 @@ public static class Core
             Logging.Fatal("UNABLE TO FIND CANVAS IN CURRENT SCENE");
             return;
         }
-        else
-        {
-            switch(levelName) 
-            { 
-                case "Intro": { break; }
-                case "Main Menu":
+
+        switch(levelName) 
+        { 
+            case "Main Menu":
+            {
+                if(Core.wasLanguageReset)
                 {
-                    if(Core.wasLanguageReset)
-                    {
-                        Core.wasLanguageReset = false;
-                        MonoSingleton<HudMessageReceiver>.Instance.SendHudMessage("<color=orange>The currently set language file could not be loaded.\nLanguage has been reset to English to avoid problems.</color>");
-                    }
+                    Core.wasLanguageReset = false;
+                    MonoSingleton<HudMessageReceiver>.Instance?.SendHudMessage("<color=orange>The currently set language file could not be loaded.\nLanguage has been reset to English to avoid problems.</color>");
+                }
 
-                    PatchFrontEnd(canvasObj);
+                MainMenu.Patch(canvasObj);
+                Options.Patch(ref canvasObj);
 
-                    break;
-                    }
-
-                default:
-                {
-                    if (isUsingEnglish())
-                    {
-                        Logging.Warn("Current language is English, not patching.");
-                        return;
-                    }
-                    
-                    Logging.Message("Regular scene");
-                    Logging.Message("Attempting to patch base elements");
-                    try{_PauseMenu.PatchPauseMenu(ref canvasObj);} catch(Exception e){Console.WriteLine(e.ToString());}
-                    try{Cheats.PatchCheatConsentPanel(ref canvasObj);;} catch(Exception e){Console.WriteLine(e.ToString());}
-                    try{Sandbox.PatchAlterMenu();} catch(Exception e){ Console.WriteLine(e.ToString());}
-                    try{HUDMessages.PatchDeathScreen(ref canvasObj);} catch(Exception e){Console.WriteLine(e.ToString());}
-                    try{LevelStatWindow.PatchStats(ref canvasObj);} catch(Exception e){Console.WriteLine(e.ToString());}
-                    try{HUDMessages.PatchMisc(ref canvasObj);} catch(Exception e){Console.WriteLine(e.ToString());}
-                    try{Options.Patch(ref canvasObj);} catch(Exception e){Console.WriteLine(e.ToString());}
-    
-                    Logging.Message("Base elements patched");
-                    }
-                    
-                    
-                    if (levelName.Contains("Tutorial"))
-                        { 
-                            Logging.Message("Tutorial");
-                        }
-                        else if (levelName.Contains("-S"))
-                        {
-                            Logging.Message("Secret");
-                            SecretLevels.Patch(ref canvasObj);
-                        }
-                        if(levelName.Contains("0-"))
-                        { 
-                            Logging.Message("Prelude");
-                            Prelude.Patch(ref canvasObj);
-                        }
-                        else if(levelName.Contains("1-") || levelName.Contains("2-") || levelName.Contains("3-"))
-                        {
-                            Logging.Message("Act 1");
-                            Act1.PatchAct1(ref canvasObj);
-                        }
-                        else if(levelName.Contains("4-") || levelName.Contains("5-") || levelName.Contains("6-"))
-                        {
-                            Logging.Message("Act 2");
-                            Act2.PatchAct2(ref canvasObj);
-                        }
-                        else if(levelName.Contains("7-") || levelName.Contains("8-") || levelName.Contains("9-"))
-                        {
-                            Logging.Message("Act 3");
-                            if(LanguageManager.CurrentLanguage.act3 != null)
-                            {
-                                Act3.PatchAct3(ref canvasObj);
-                            }
-                            else
-                            {
-                                Logging.Warn("Category is not found in the language file!");
-                            }
-                        }
-                        else if (levelName.Contains("P-"))
-                        {
-                            Logging.Message("Prime");
-                            PrimeSanctum.Patch();
-                        }
-                        else if (levelName == "uk_construct")
-                        { 
-                            Logging.Message("Sandbox");
-                            Sandbox.Patch(ref canvasObj);
-                        }
-                        else if (levelName == "Endless")
-                        {
-                            Logging.Message("CyberGrind");
-                            CyberGrind.PatchCg();
-                        }
-                        else if (levelName.Contains("Intermission") || levelName.Contains("EarlyAccessEnd"))
-                        {
-                            Logging.Message("Intermission");
-                            Intermission.Patch(ref canvasObj);
-                        }
-                        else if (levelName == "CreditsMuseum2")
-                        {
-                            Logging.Message("DevMuseum");
-                            DevMuseum.Patch();
-                        }
-                    break;
+                break;
             }
+            default:
+            {
+                if (isUsingEnglish())
+                {
+                    Logging.Warn("Current language is English, not patching.");
+                    return;
+                }
+                
+                Logging.Message("Regular scene");
+
+                PatchingBaseElements(ref canvasObj);
+
+                HandleLevelPatching(levelName, ref canvasObj);
+
+                break;
+            }
+        }
+    }
+
+    private static void PatchingBaseElements(ref GameObject canvasObj)
+    {
+        Logging.Message("Attempting to patch base elements");
+        try{_PauseMenu.PatchPauseMenu(ref canvasObj);} catch(Exception e){Console.WriteLine(e.ToString());}
+        try{Cheats.PatchCheatConsentPanel(ref canvasObj);;} catch(Exception e){Console.WriteLine(e.ToString());}
+        try{Sandbox.PatchAlterMenu();} catch(Exception e){ Console.WriteLine(e.ToString());}
+        try{HUDMessages.PatchDeathScreen(ref canvasObj);} catch(Exception e){Console.WriteLine(e.ToString());}
+        try{LevelStatWindow.PatchStats(ref canvasObj);} catch(Exception e){Console.WriteLine(e.ToString());}
+        try{HUDMessages.PatchMisc(ref canvasObj);} catch(Exception e){Console.WriteLine(e.ToString());}
+        try{Options.Patch(ref canvasObj);} catch(Exception e){Console.WriteLine(e.ToString());}
+
+        Logging.Message("Base elements patched");
+    }
+
+    private static void HandleLevelPatching(string levelName, ref GameObject canvasObj)
+    {
+        if (levelName.Contains("Tutorial"))
+        { 
+            Logging.Message("Tutorial");
+        }
+        else if (levelName.Contains("-S"))
+        {
+            Logging.Message("Secret");
+            SecretLevels.Patch(ref canvasObj);
+        }
+        if(levelName.Contains("0-"))
+        { 
+            Logging.Message("Prelude");
+            Prelude.Patch(ref canvasObj);
+        }
+        else if(levelName.Contains("1-") || levelName.Contains("2-") || levelName.Contains("3-"))
+        {
+            Logging.Message("Act 1");
+            Act1.PatchAct1(ref canvasObj);
+        }
+        else if(levelName.Contains("4-") || levelName.Contains("5-") || levelName.Contains("6-"))
+        {
+            Logging.Message("Act 2");
+            Act2.PatchAct2(ref canvasObj);
+        }
+        else if(levelName.Contains("7-") || levelName.Contains("8-") || levelName.Contains("9-"))
+        {
+            Logging.Message("Act 3");
+            if(LanguageManager.CurrentLanguage.act3 != null)
+            {
+                Act3.PatchAct3(ref canvasObj);
+            }
+            else
+            {
+                Logging.Warn("Category is not found in the language file!");
+            }
+        }
+        else if (levelName.Contains("P-"))
+        {
+            Logging.Message("Prime");
+            PrimeSanctum.Patch();
+        }
+        else if (levelName == "uk_construct")
+        { 
+            Logging.Message("Sandbox");
+            Sandbox.Patch(ref canvasObj);
+        }
+        else if (levelName == "Endless")
+        {
+            Logging.Message("CyberGrind");
+            CyberGrind.PatchCg();
+        }
+        else if (levelName.Contains("Intermission") || levelName.Contains("EarlyAccessEnd"))
+        {
+            Logging.Message("Intermission");
+            Intermission.Patch(ref canvasObj);
+        }
+        else if (levelName == "CreditsMuseum2")
+        {
+            Logging.Message("DevMuseum");
+            DevMuseum.Patch();
         }
     }
 
