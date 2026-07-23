3ceaad6c0ce458d6407b59ecc3cdda83e62eb773
greycsont <RuiD2019@outlook.com>
2026-07-23 04:40:24 +0100

    Cleaned up Nailgun's patch - Removed detach of TMPtwin since it's already fixed in 1.2.0

--- UltrakULL/Translations/HarmonyPatches/Nailgun.cs
+++ UltrakULL/Translations/HarmonyPatches/Nailgun.cs
@@ -18,99 +18,65 @@ namespace UltrakULL.Harmony_Patches;
 [HarmonyPatch(typeof(Nailgun))]
 public static class NailgunPatch
 {
-    //Magnet Nailgun/Sawblade Launcher ammo text is disappers without this.
-    [HarmonyPatch("Start"), HarmonyPostfix]
-    public static void NailgunPostfix(Nailgun __instance, TMP_Text ___statusText)
+    /// <summary>
+    /// Update the Zap's RECHARING text
+    /// The reason to not put it in the zap patch
+    /// Since GetComponentInChildren() is a performance breaker
+    /// </summary>
+    /// <param name="__instance">Nailgun</param>
+    [HarmonyPatch(nameof(Nailgun.Start)), HarmonyPostfix]
+    public static void StartPostfix(Nailgun __instance)
     {
-        if (__instance.gameObject.name.ToLower().Contains("magnet"))
-        {
-            // the tmp will not shown so i just detach it
-            __instance.ammoText.GetComponent<TMPTwin>()?.Detach();
-        }
+        var rechargeText = __instance.rechargingOverlay?.GetComponentInChildren<TMP_Text>(true);
+
+        if (rechargeText != null)
+            rechargeText.text = LanguageManager.CurrentLanguage.weapon.weapon_nailgunZapperRecharging;
     }
-    [HarmonyPatch("Update"), HarmonyPostfix]
-    public static void NailgunUpdatePostfix(Nailgun __instance, WeaponCharges ___wc)
+
+    /// <summary>
+    /// This one is for arabic-indic numbers
+    /// </summary>
+    /// <param name="__instance">Nailgun</param>
+    [HarmonyPatch(nameof(Nailgun.Update)), HarmonyPostfix]
+    public static void UpdatePostfix(Nailgun __instance)
     {
-        //This one is for arabic numbers
-        if(__instance.variation == 1 && LanguageManager.CurrentLanguage.metadata.langHinduNumbers)
+        var nailgun = __instance;
+        if(nailgun.variation == 1 && LanguageManager.CurrentLanguage.metadata.langHinduNumbers)
         {
-            if (__instance.altVersion) 
+            if (nailgun.altVersion) 
             {
-                __instance.ammoText.text = ArabicFixerTool.FixLine(Mathf.RoundToInt(___wc.naiSaws).ToString()).ToString();
+                nailgun.ammoText.text = ArabicFixerTool.FixLine(Mathf.RoundToInt(nailgun.wc.naiSaws).ToString()).ToString();
             }
             else
             {
-                __instance.ammoText.text = ArabicFixerTool.FixLine(Mathf.RoundToInt(___wc.naiAmmo).ToString()).ToString();
+                __instance.ammoText.text = ArabicFixerTool.FixLine(Mathf.RoundToInt(nailgun.wc.naiAmmo).ToString()).ToString();
             }
         }
-        
     }
-
-    static bool enablezapperfix = true;
-    [HarmonyPatch("UpdateZapHud"), HarmonyPostfix]
-    public static void zapperpostfix(Nailgun __instance, Zapper ___currentZapper, CameraController ___cc, TMP_Text ___statusText, GameObject ___rechargingOverlay)
+    
+    /// <summary>
+    /// Modify the Zap's HUD text
+    /// </summary>
+    /// <param name="__instance">Nailgun</param>
+    [HarmonyPatch(nameof(Nailgun.UpdateZapHud)), HarmonyPostfix]
+    public static void Za(Nailgun __instance)
     {
-        try
-        {
-            if (!enablezapperfix) { return; }
-            if (___currentZapper == null)
-            {
-                if (!__instance.gameObject.name.ToLower().Contains("zapper"))
-                {
-                    return;
-                }
-                if (___rechargingOverlay.activeSelf)
-                {
-                    TextMeshProUGUI rechargeText = GetTextMeshProUGUI(FindDescendant(___rechargingOverlay, "Text (TMP)"));
-                    //RECHARGING...
-                    rechargeText.text = LanguageManager.CurrentLanguage.weapon.weapon_nailgunZapperRecharging;
-                }
-                RaycastHit raycastHit;
-                EnemyIdentifierIdentifier enemyIdentifierIdentifier;
-                float target;
-                if (Physics.Raycast(___cc.GetDefaultPos(), ___cc.transform.forward, out raycastHit,
-                    float.PositiveInfinity, LayerMaskDefaults.Get(LMD.EnemiesAndEnvironment)) &&
-                    raycastHit.collider.gameObject.layer != 8 && raycastHit.collider.gameObject.layer != 24 &&
-                    raycastHit.collider.TryGetComponent<EnemyIdentifierIdentifier>(out enemyIdentifierIdentifier) &&
-                    enemyIdentifierIdentifier.eid && !enemyIdentifierIdentifier.eid.dead)
-                {
-                    if (raycastHit.distance < __instance.zapper.maxDistance - 5f)
-                    {
-                        target = 1f - (__instance.zapper.maxDistance - 5f - raycastHit.distance) / (__instance.zapper.maxDistance - 5f);
-                        //READY
-                        ___statusText.text = LanguageManager.CurrentLanguage.weapon.weapon_nailgunZapperReady;
-                    }
-                    else
-                    {
-                        //TOO FAR | OUT OF RANGE
-                        ___statusText.text = (__instance.altVersion ? LanguageManager.CurrentLanguage.weapon.weapon_nailgunZapperAlternateTooFar : LanguageManager.CurrentLanguage.weapon.weapon_nailgunZapperOutOfRange);
-                    }
-                }
-                else
-                {
-                    //NULL | NO TARGET
-                    ___statusText.text = (__instance.altVersion ? LanguageManager.CurrentLanguage.weapon.weapon_nailgunZapperAlternateNull : LanguageManager.CurrentLanguage.weapon.weapon_nailgunZapperNoTarget);
-                }
-                return;
-            }
-            if (___currentZapper.distance > ___currentZapper.maxDistance || ___currentZapper.raycastBlocked)
-            {
-                //BLOCKED (TOO FAR | OUT OF RANGE)
-                ___statusText.text = (___currentZapper.raycastBlocked ? LanguageManager.CurrentLanguage.weapon.weapon_nailgunZapperBlocked : (__instance.altVersion ? LanguageManager.CurrentLanguage.weapon.weapon_nailgunZapperAlternateTooFar : LanguageManager.CurrentLanguage.weapon.weapon_nailgunZapperOutOfRange));
-                return;
-            }
-            //DISTANCE:
-            ___statusText.text = (__instance.altVersion ? "" : LanguageManager.CurrentLanguage.weapon.weapon_nailgunZapperDistance) + ___currentZapper.distance.ToString("f1");
-        }
-        catch (Exception e)
-        {
-            Logging.Warn("Failed to Patch zapper display!");
-            if (LanguageManager.CurrentLanguage.weapon == null)
-            { Logging.Warn("Category \"Weapon\" is missing from the language file!"); }
-            Logging.Warn("Disabling this for prevent spam");
-            Logging.Warn(e.ToString());
-            enablezapperfix = false;
-        }
-    }
+        var nailgun = __instance;
+        var language = LanguageManager.Current.Json.weapon;
 
+        var hudText = nailgun.statusText.text;
+
+        nailgun.statusText.text = hudText switch
+        {
+            "READY"        => language.weapon_nailgunZapperReady,
+            "TOO FAR"      => language.weapon_nailgunZapperAlternateTooFar,
+            "OUT OF RANGE" => language.weapon_nailgunZapperOutOfRange,
+            "NULL"         => language.weapon_nailgunZapperAlternateNull,
+            "NO TARGET"    => language.weapon_nailgunZapperNoTarget,
+            "BLOCKED"      => language.weapon_nailgunZapperBlocked,
+            _ when hudText.StartsWith("DISTANCE: ", StringComparison.Ordinal)
+                => language.weapon_nailgunZapperDistance + hudText.Substring("DISTANCE: ".Length),
+            _ => hudText,
+        };
+    }
 }
