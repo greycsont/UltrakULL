f959348fe2a29c62eb243e169e4217797656bc1e
greycsont <RuiD2019@outlook.com>
2026-07-23 05:09:48 +0100

    Cleaned up MoneyText patch - fixed? missing space between number and P

--- UltrakULL/Translations/HarmonyPatches/MoneyTextPatch.cs
+++ UltrakULL/Translations/HarmonyPatches/MoneyTextPatch.cs
@@ -6,25 +6,31 @@ using UltrakULL.json;
 
 namespace UltrakULL.Harmony_Patches;
 
-[HarmonyPatch(typeof(MoneyText), "DivideMoney")]
+[HarmonyPatch(typeof(MoneyText))]
 public static class MoneyTextPatch
 {
-    [HarmonyPostfix]
+    /// <summary>
+    /// "LIKE, A LOT OF "
+    /// </summary>
+    /// <param name="dosh"></param>
+    /// <param name="__result"></param>
+    [HarmonyPatch(nameof(MoneyText.DivideMoney))] [HarmonyPostfix]
     public static void DivideMoneyPatch(int dosh, ref string __result)
     {
         if (dosh > 1000000000)
-        {
             __result = LanguageManager.CurrentLanguage.shop.shop_lotsOfMoney;
-        }
     }
-}
-[HarmonyPatch(typeof(MoneyText), "UpdateMoney")]
-public static class UpdateMoneyPatch
-{
-    [HarmonyPostfix]
+
+    /// <summary>
+    /// If you have any question of that single space
+    /// Please check the source code of ULTRAKILL
+    /// </summary>
+    /// <param name="__instance"></param>
+    [HarmonyPatch(nameof(MoneyText.UpdateMoney))] [HarmonyPostfix]
     public static void UpdateMoneyPostfix(MoneyText __instance)
     {
-        TextMeshProUGUI tmtext = __instance.GetComponent<TextMeshProUGUI>();
-        tmtext.text = MoneyText.DivideMoney(GameProgressSaver.GetMoney()) + "<color=#FF4343>" + LanguageManager.CurrentLanguage.shop.shop_moneyCount + "</color>";
+        __instance.text.text = 
+            MoneyText.DivideMoney(GameProgressSaver.GetMoney()) 
+            + " <color=#FF4343>" + LanguageManager.CurrentLanguage.shop.shop_moneyCount + "</color>";
     }
 }
