aeadc847498f3231a362a3cd091d824416745ee2
greycsont <RuiD2019@outlook.com>
2026-07-29 05:36:30 +0100

    Add [CallerFilePath] to Logging.cs to help write debug logs

--- src/Core/Logging.cs
+++ src/Core/Logging.cs
@@ -1,41 +1,34 @@
-﻿
+﻿using System.IO;
+using System.Runtime.CompilerServices;
 using BepInEx.Logging;
 
 namespace UltrakULL;
 
-public static class Logging 
+public static class Logging
 {
     public static ManualLogSource UllLogger = Logger.CreateLogSource("ULL LOGGING");
 
-    public static void Debug(string text)
-    {
-        UllLogger.LogDebug(text);
-    }
-    
-    public static void Message(string text)
-    {
-        UllLogger.LogMessage(text);
-    }
-    
-    public static void Warn(string text)
-    {
-        UllLogger.LogWarning(text);
-    }
-    
-    public static void Error(string text)
-    {
-        UllLogger.LogError(text);
-    }
-    
-    public static void Fatal(string text)
-    {
-        UllLogger.LogFatal(text);
-    }
-    
-    public static void Info(string text)
-    {
-        UllLogger.LogInfo(text);
-    }
+    public static void Debug(string text, bool addCallerFilePath = false, [CallerFilePath] string callerFilePath = "") 
+        => UllLogger.LogDebug(TryAddCallerName(text, addCallerFilePath, callerFilePath));
 
-    
+    public static void Message(string text, bool addCallerFilePath = false, [CallerFilePath] string callerFilePath = "") 
+        => UllLogger.LogMessage(TryAddCallerName(text, addCallerFilePath, callerFilePath));
+
+    public static void Warn(string text, bool addCallerFilePath = false, [CallerFilePath] string callerFilePath = "") 
+        => UllLogger.LogWarning(TryAddCallerName(text, addCallerFilePath, callerFilePath));
+
+    public static void Error(string text, bool addCallerFilePath = false, [CallerFilePath] string callerFilePath = "") 
+        => UllLogger.LogError(TryAddCallerName(text, addCallerFilePath, callerFilePath));
+
+    public static void Fatal(string text, bool addCallerFilePath = false, [CallerFilePath] string callerFilePath = "") 
+        => UllLogger.LogFatal(TryAddCallerName(text, addCallerFilePath, callerFilePath));
+
+    public static void Info(string text, bool addCallerFilePath = false, [CallerFilePath] string callerFilePath = "") 
+        => UllLogger.LogInfo(TryAddCallerName(text, addCallerFilePath, callerFilePath));
+
+    private static string TryAddCallerName(string text, bool addCallerFilePath, string callerFilePath) 
+        => addCallerFilePath ? $"[{GetFileName(callerFilePath)}] {text}" : text;
+
+    private static string GetFileName(string callerFilePath) 
+        => Path.GetFileNameWithoutExtension(callerFilePath);
 }
