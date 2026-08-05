e8cdbec9a9e5a10ddede417f2a8cbd88614b0af0
greycsont <RuiD2019@outlook.com>
2026-08-05 02:49:29 +0100

    Add SetOutline() in TMPTextExtensions

--- src/Localization/Text/Compatibility/TMPTextExtensions.cs
+++ src/Localization/Text/Compatibility/TMPTextExtensions.cs
@@ -29,5 +29,20 @@ public static class TMPTextExtensions
             value.UpdateMeshPadding();
             value.ForceMeshUpdate(true);
         }
+
+        public void SetOutline(Color color, float width)
+        {
+            if (value == null)
+                return;
+
+            Material material = value.fontMaterial;
+            material.EnableKeyword(ShaderUtilities.Keyword_Outline);
+            material.SetColor(ShaderUtilities.ID_OutlineColor, color);
+            material.SetFloat(ShaderUtilities.ID_OutlineWidth, width);
+            ShaderUtilities.UpdateShaderRatios(material);
+
+            value.UpdateMeshPadding();
+            value.ForceMeshUpdate(true);
+        }
     }
 }
