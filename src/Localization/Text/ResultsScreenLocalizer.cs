3f4b093a097540c17d47397573b9f235c9411e3d
greycsont <RuiD2019@outlook.com>
2026-08-18 15:03:48 +0100

    Adds ResultScreen's title will shrinking it it's too long

--- src/Localization/Text/ResultsScreenLocalizer.cs
+++ src/Localization/Text/ResultsScreenLocalizer.cs
@@ -47,5 +47,9 @@ public static class ResultsScreenLocalizer
         GetTextMeshProUGUI(
             FindDescendant(panel, "Total Points", "Text (1)")).text =
             LanguageManager.CurrentLanguage.cyberGrind.cybergrind_total + ":";
+
+        var titleText = FindComponent<TextMeshProUGUI>(panel, "Title", "Text");
+        titleText.overflowMode = TextOverflowModes.Overflow;
+        titleText.enableWordWrapping = false;
     }
 }
