c70320ed83bd9308c3508c0a88af174e0a3ba601
greycsont <RuiD2019@outlook.com>
2026-08-20 23:02:39 +0100

    Fixed the Cybergrind's levelstats window are translated to wrong field

--- src/Localization/Text/Scenes/LevelStatWindow.cs
+++ src/Localization/Text/Scenes/LevelStatWindow.cs
@@ -1,46 +1,38 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 using UltrakULL.json;
 
 using static UltrakULL.SceneObjects;
 
+using static UltrakULL.json.LanguageManager;
+
 namespace UltrakULL;
 
-[NeedRework]
 public static class LevelStatWindow
 {
     public static void PatchStats(GameObject canvasObj)
     {
         GameObject levelStatsWindow = FindDescendant(canvasObj, "Level Stats Controller", "Level Stats (1)");
-        
-        TextMeshProUGUI levelName = GetTextMeshProUGUI(FindDescendant(levelStatsWindow, "Title"));
-        levelName.text = LevelNames.GetDiscordLevelName(GetCurrentSceneName());
 
-        //Secret levels will only have a timer, or something else.
-        TextMeshProUGUI timeName = GetTextMeshProUGUI(FindDescendant(levelStatsWindow, "Time Title"));
-        timeName.text = LanguageManager.CurrentLanguage.misc.levelstats_time; 
+        levelStatsWindow.Localize<TextMeshProUGUI>(CurrentLanguage.misc.levelstats_time, "Time Title");
+        levelStatsWindow.Localize<TextMeshProUGUI>(
+            GetCurrentSceneName() == "" 
+            ? CurrentLanguage.misc.levelstats_kills : CurrentLanguage.cyberGrind.cybergrind_wave, 
+            "Kills Title");
 
-        TextMeshProUGUI killsName = GetTextMeshProUGUI(FindDescendant(levelStatsWindow, "Kills Title"));
-        killsName.text = LanguageManager.CurrentLanguage.misc.levelstats_kills;
+        levelStatsWindow.Localize<TextMeshProUGUI>(
+            GetCurrentSceneName() == "" 
+            ? CurrentLanguage.misc.levelstats_style : CurrentLanguage.cyberGrind.cybergrind_enemiesRemaining, 
+            "Style Title");
+        levelStatsWindow.Localize<TextMeshProUGUI>(CurrentLanguage.misc.levelstats_secrets, "Secrets Title");
 
-        TextMeshProUGUI styleName = GetTextMeshProUGUI(FindDescendant(levelStatsWindow, "Style Title"));
-        styleName.text = LanguageManager.CurrentLanguage.misc.levelstats_style;
-
-        TextMeshProUGUI secretsName = GetTextMeshProUGUI(FindDescendant(levelStatsWindow, "Secrets Title"));
-        secretsName.text = LanguageManager.CurrentLanguage.misc.levelstats_secrets;
-
-        TextMeshProUGUI challengesName = GetTextMeshProUGUI(FindDescendant(levelStatsWindow, "Challenge Title"));
-        challengesName.text = LanguageManager.CurrentLanguage.misc.levelstats_challenge;
-
-        TextMeshProUGUI assistsName = GetTextMeshProUGUI(FindDescendant(levelStatsWindow, "Assists Title"));
-        assistsName.text = LanguageManager.CurrentLanguage.misc.levelstats_majorAssists;
+        levelStatsWindow.Localize<TextMeshProUGUI>(CurrentLanguage.misc.levelstats_challenge, "Challenge Title");
+        levelStatsWindow.Localize<TextMeshProUGUI>(CurrentLanguage.misc.levelstats_majorAssists, "Assists Title");
 
         if (GetCurrentSceneName() == "Level 4-S")
         {
-            TextMeshProUGUI cratesName = GetTextMeshProUGUI(FindDescendant(levelStatsWindow, "Crates Counter"));
-            cratesName.text = LanguageManager.CurrentLanguage.misc.levelstats_boxes;
+            levelStatsWindow.Localize<TextMeshProUGUI>(CurrentLanguage.misc.levelstats_boxes, "Crates Counter");
         }
     }
 }
