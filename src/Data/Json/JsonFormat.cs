bfc95a7a4bce12596a9daed341d99be971d5e455
greycsont <RuiD2019@outlook.com>
2026-09-19 01:05:44 +0100

    Add ability to translate the ERROR/OK in the tutorial
    - add new field tutorial_calibrationError and tutorial_calibrationOk in
    JsonFormat
    - add transpiler to IntroText.TextAppear()

--- src/Data/Json/JsonFormat.cs
+++ src/Data/Json/JsonFormat.cs
@@ -2547,6 +2547,8 @@ public class Tutorial
     public string tutorial_calibrationMechanics;
     public string tutorial_calibrationComplete1;
     public string tutorial_calibrationComplete2;
+    public string tutorial_calibrationError;
+    public string tutorial_calibrationOk;
     public string tutorial_introReminder1;
     public string tutorial_introReminder2;
     public string tutorial_systemsOperational;
--- src/Localization/Scenes/Tutorial/IntroText.cs
+++ src/Localization/Scenes/Tutorial/IntroText.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
 using UltrakULL.json;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-
+using static UltrakULL.json.LanguageManager;
 using static UltrakULL.SceneObjects;
 
 namespace UltrakULL.Harmony_Patches;
@@ -14,10 +17,15 @@ namespace UltrakULL.Harmony_Patches;
 [HarmonyPatch(typeof(IntroText))]
 public static class LocalizeIntroText
 {
+    public static MethodInfo LocalizeCalibrationStatusTextMethodInfo = AccessTools.Method(
+        typeof(LocalizeIntroText),
+        nameof(LocalizeIntroText.LocalizeCalibrationStatusText)
+    );
+
     [HarmonyPatch(nameof(IntroText.Start))] [HarmonyPrefix]
     public static bool IntroTextStart_MyPatch(IntroText __instance, TMP_Text ___txt, string ___fullString)
     {
-        if(LanguageManager.IsEnglish)
+        if (LanguageManager.IsEnglish)
         {
             return true;
         }
@@ -32,5 +40,38 @@ public static class LocalizeIntroText
         ___txt.text = ___fullString;
 
         return true;
+    }
+
+    [HarmonyPatch(nameof(IntroText.TextAppear), MethodType.Enumerator)] [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> TextAppearTranspiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var matcher = new CodeMatcher(instructions);
+        return matcher.MatchForward(false, 
+            new CodeMatch(i => i.opcode == OpCodes.Ldstr 
+            && (string)i.operand == "<color=red>ERROR</color>")
+        )
+        .Advance(1)
+        .Insert(new CodeInstruction(OpCodes.Call, LocalizeCalibrationStatusTextMethodInfo))
+        .MatchForward(false,
+            new CodeMatch(i => i.opcode == OpCodes.Ldstr
+            && (string)i.operand == "<color=green>OK</color>")
+        )
+        .Advance(1)
+        .Insert(new CodeInstruction(OpCodes.Call, LocalizeCalibrationStatusTextMethodInfo))
+        .InstructionEnumeration();
+    }
+
+    public static string LocalizeCalibrationStatusText(string input)
+    {
+        if (LanguageManager.IsEnglish) return input;
+        
+        var localizedStatus = input switch
+        {
+            "<color=red>ERROR</color>" => "<color=red>" + CurrentLanguage.tutorial.tutorial_calibrationError + "</color>",
+            "<color=green>OK</color>" => "<color=green>" + CurrentLanguage.tutorial.tutorial_calibrationOk + "</color>",
+            _ => input
+        };
+
+        return localizedStatus.Or(input);
     }
 }
