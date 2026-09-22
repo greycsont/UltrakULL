f3df2bce5c1eaee9d0dfa097db64793430cf9ec3
greycsont <RuiD2019@outlook.com>
2026-09-22 09:56:19 +0100

    Makes Sisyphus's subtitle localizer to using InjectLocalizer2

--- src/Localization/Voice&Subtitle/SisyphusPrime/SisyphusPrimeSubtitlesSwap.cs
+++ src/Localization/Voice&Subtitle/SisyphusPrime/SisyphusPrimeSubtitlesSwap.cs
@@ -1,11 +1,19 @@
 using System.Collections.Generic;
+using System.Reflection;
 using HarmonyLib;
+using UltrakULL.json;
 
 namespace UltrakULL.Harmony_Patches.Subtitles;
 
 [HarmonyPatch(typeof(SisyphusPrime))]
 public class SisyphusPrimeSubtitlesSwap
 {
+	public static MethodInfo LocalizeSisyphusSubtitleMethodInfo = AccessTools.Method(
+		typeof(SisyphusPrimeSubtitlesSwap),
+		nameof(SisyphusPrimeSubtitlesSwap.LocalizeSisyphusSubtitle),
+		new[] { typeof(string) }
+	);
+
 	[HarmonyTranspiler]
 	[HarmonyPatch(nameof(SisyphusPrime.Enrage))]
 	[HarmonyPatch(nameof(SisyphusPrime.Taunt))]
@@ -15,6 +23,25 @@ public class SisyphusPrimeSubtitlesSwap
 	[HarmonyPatch(nameof(SisyphusPrime.ExplodeAttack))]
 	private static IEnumerable<CodeInstruction> SisyphusPrime_SubtitlePatch(IEnumerable<CodeInstruction> instructions)
 	{
-		return SubtitleLocalizer.InjectLocalize(instructions);
+		return SubtitleLocalizer.InjectLocalize2(instructions, LocalizeSisyphusSubtitleMethodInfo);
+	}
+
+	public static string LocalizeSisyphusSubtitle(string input)
+	{
+		if (LanguageManager.IsEnglish)
+			return input;
+
+		var s = LanguageManager.CurrentLanguage?.subtitles;
+
+		return (input switch
+		{
+			"Nice try!" => s.subtitles_sisyphusPrime_attack1,
+			"BE GONE!" => s.subtitles_sisyphusPrime_attack2,
+			"You can't escape!" => s.subtitles_sisyphusPrime_attack3,
+			"DESTROY!" => s.subtitles_sisyphusPrime_attack4,
+			"This will hurt." => s.subtitles_sisyphusPrime_attack5,
+			"YES! That's it!" => s.subtitles_sisyphusPrime_phaseChange,
+			_ => input,
+		}).Or(input);
 	}
 }
